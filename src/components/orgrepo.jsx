d99c5b7e7cabf9eb5cf43834f9e8d18426053bfd
ryantruong25 <75468904+ryan-truong@users.noreply.github.com>
2023-05-30 13:03:38 -0700

    Updated feature

--- src/components/orgrepo.jsx
+++ src/components/orgrepo.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import axios from 'axios';
 
-import RepoCard from './repocard'
+// import CommitCard from './repocard'
 import './orgpage.css'
 
 const OrgRepo = () => {
@@ -11,7 +11,7 @@ const OrgRepo = () => {
     const location = useLocation();
 
     useEffect(() => {
-      axios.get(`https://api.github.com/repos${location.pathname}/git/commits`)
+      axios.get(`https://api.github.com/repos${location.pathname}/commits`)
       .catch(function (error){
           console.log(error.response);
       })
@@ -27,17 +27,7 @@ const OrgRepo = () => {
           <>
               <h1>{location.pathname.split("/").slice(-1)} commits</h1>
               <div className = 'container'>
-                  {post.map( (entry) => (
-                      <RepoCard key={entry.id}
-                      name={entry.name}
-                      language={entry.language}
-                      description={entry.description}
-                      star={entry.stargazers_count}
-                      fork={entry.forks_count}
-                      date={entry.created_at.substring(0,entry.created_at.indexOf("T"))}
-                      />
-                  )
-                  )}
+    
               </div>
           </>
       )
